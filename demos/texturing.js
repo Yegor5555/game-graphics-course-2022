f6fcad2d3a542d25c517e7b7c18dbc0129c15fbc
Yegor5555 <jegor.polistsuk@gmail.com>
2022-11-21 20:10:17 +0200

    Only changed the cube itself and background

--- demos/texturing.js
+++ demos/texturing.js
@@ -117,7 +117,7 @@ async function loadTexture(fileName) {
     return await createImageBitmap(await (await fetch("images/" + fileName)).blob());
 }
 
-const tex = await loadTexture("abstract.jpg");
+const tex = await loadTexture("ownimage.jpg");
 let drawCall = app.createDrawCall(program, vertexArray)
     .texture("tex", app.createTexture2D(tex, tex.width, tex.height, {
         magFilter: PicoGL.LINEAR,
@@ -129,12 +129,12 @@ let drawCall = app.createDrawCall(program, vertexArray)
 
 let skyboxDrawCall = app.createDrawCall(skyboxProgram, skyboxArray)
     .texture("cubemap", app.createCubemap({
-        negX: await loadTexture("stormydays_bk.png"),
-        posX: await loadTexture("stormydays_ft.png"),
-        negY: await loadTexture("stormydays_dn.png"),
-        posY: await loadTexture("stormydays_up.png"),
-        negZ: await loadTexture("stormydays_lf.png"),
-        posZ: await loadTexture("stormydays_rt.png")
+        negX: await loadTexture("nx.png"),
+        posX: await loadTexture("ny.png"),
+        negY: await loadTexture("nz.png"),
+        posY: await loadTexture("px.png"),
+        negZ: await loadTexture("py.png"),
+        posZ: await loadTexture("pz.png")
     }));
 
 function draw(timems) {
